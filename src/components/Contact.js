6abf206d0d879a0059eb344427018fe47d751443
EricNguyen99 <vanthin7111999@gmail.com>
2020-03-29 23:50:43 +0700

    add prompt object

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import {Prompt} from 'react-router-dom';
 
 class Contact extends Component {
+   constructor(props){
+      super(props);
+      this.state = {
+         isChecked : false
+      }
+   }
+
+   onClick = (isChecked) => {
+      this.setState({
+         isChecked : isChecked
+      })
+   }
    render() {
+      var {isChecked} = this.state;
       return (
          <div>
-            Đây là trang liên hệ
+            Đây là trang liên hệ <br/>
+            <button 
+               type="button" 
+               class="btn btn-info"
+               onClick = {()=>this.onClick(false)}
+            >Cho phép</button><br/>
+            <button 
+               type="button" 
+               class="btn btn-danger"
+               onClick = {()=>this.onClick(true)}
+            >Không cho phép</button><br/>
+            <Prompt
+               when={isChecked}
+               message={location => (`Bạn chắc chắn muốn đi đến ${location.pathname}`)}
+            />
          </div>
       );
    }
